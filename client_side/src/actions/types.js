48e9eb8fee8e5fa9c989ce1947fae76f5f9d99cf
RAJ RATHOD <68474970+ArerehRaj@users.noreply.github.com>
2021-11-30 14:15:33 +0530

    Update types.js

--- client_side/src/actions/types.js
+++ client_side/src/actions/types.js
@@ -1,5 +1,6 @@
 export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
+export const FEEDBACK_USER = 'feedback_user';
 export const AUTH_USER = 'auth_user';
 export const LOGOUT_USER = 'logout_user';
 
